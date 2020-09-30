8a882d1a05d2a9af45a03b7b7d13559b8ba9ec76
Rajat Agarwal <rajat18agarwal@gmail.com>
2020-09-30 21:55:22 +0530

    commmit

--- EnigmaMachine/EnigmaMachine/EnigmaDecrypter.cpp
+++ EnigmaMachine/EnigmaMachine/EnigmaDecrypter.cpp
@@ -14,6 +14,26 @@
 EnigmaDecrypter::EnigmaDecrypter() { }
 
 
+/*bool EnigmaDecrypter::offsetIterator(){
+	for(int i = 0; i<26; i++){
+		for(int j = 0; j<26; j++){
+			for(int k = 0; k<26; k++){
+				int arr[]={k, j, i};
+				_enigmaMachine.setRotorOffset(arr);
+				std::string decoded = _enigmaMachine.encode(_crib);
+				if(decoded == _plaintxt){
+					_config.offsets[0]=k;
+					_config.offsets[1]=j;
+					_config.offsets[2]=i;
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+*/
+
 bool EnigmaDecrypter::offsetIterator(){
 	for(int i = 0; i<26; i++){
 		for(int j = 0; j<26; j++){
@@ -32,7 +52,6 @@ bool EnigmaDecrypter::offsetIterator(){
 	}
 	return false;
 }
-
 
 bool EnigmaDecrypter::run(std::string crib, std::string plaintxt){
 	crib = custom_utils::removeSpecial(crib);
